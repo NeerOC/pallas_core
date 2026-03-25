0d9aa0e1c6423c8ae06e7d45ee286c05e3b7d013
Neer <legitting@gmail.com>
2026-03-25 20:30:31 +0100

    Temp Debugging

---
+++ behaviors/shaman/restoration.lua
@@ -0,0 +1,97 @@
+local options = {
+    Name = "Shaman (Restoration)", -- shown as collapsing header
+
+    Widgets = {
+        { type = "text", text = "=== General ===" },
+    },
+}
+
+local function DoCombat()
+    if Me:IsCastingOrChanneling() then
+        return
+    end
+
+    if Spell.WindShear:Interrupt() then
+        return
+    end
+
+    if Spell:IsGCDActive() then
+        return
+    end
+
+    local lowest = Heal:GetLowestMember()
+    if not lowest then
+        return
+    end
+
+    if lowest.HealthPct < 50 or Me.PowerPct < 60 then return end
+
+    if not Me:HasAura("Flametongue Weapon (Passive)") and Spell.FlametongueWeapon:CastEx(Me) then
+        return
+    end
+
+    if not Me:HasAura("Lightning Shield") and Spell.LightningShield:CastEx(Me) then
+        return
+    end
+
+    if Me.HealthPct < 50 and Spell.HealingSurge:CastEx(Me) then
+        return
+    end
+
+    local target = Combat.BestTarget
+    if not target then
+        return
+    end
+
+    if Spell.EarthShock:CastEx(target) then
+        return
+    end
+
+    if Spell.LightningBolt:CastEx(target, { skipMoving = true }) then
+        return
+    end
+end
+
+local function DoHeal()
+    local lowest = Heal:GetLowestMember()
+
+    -- Cancel Healing Surge if nobody needs healing
+    if Me.CastingSpellId == Spell.HealingSurge.Id and (not lowest or lowest.HealthPct > 90) then
+        Me:StopCasting()
+    end
+
+    if Me:IsCastingOrChanneling() then
+        return
+    end
+
+    if Spell:IsGCDActive() then
+        return
+    end
+
+    if not lowest then
+        return
+    end
+
+    if lowest.HealthPct < 65 and Spell.HealingSurge:CastEx(lowest) then
+        return
+    end
+
+    if lowest.HealthPct < 90 and Spell.Riptide:CastEx(lowest) then
+        return
+    end
+
+    if lowest.HealthPct < 80 and Spell.HealingWave:CastEx(lowest) then
+        return
+    end
+
+    if Spell.PurifySpirit:Dispel(true, { DispelType.Magic, DispelType.Curse }) then
+        return
+    end
+end
+
+local behaviors = {
+    [BehaviorType.Heal] = DoHeal,
+    [BehaviorType.Combat] = DoCombat,
+}
+
+return { Options = options, Behaviors = behaviors }
